c40f3b96f6519e846bddd49ee6d0515b80d5c4eb
Dante (Kuo-Jung) Su <dante.su@broadcom.com>
2020-11-18 15:24:48 +0800

    sfputilbase.py: fix the Python crash upon IO failures (#5)
    This fixes the following failure:
    
    Nov 18 00:57:55.488171 sonic INFO pmon#supervisord: xcvrd Traceback (most recent call last):
    Nov 18 00:57:55.488171 sonic INFO pmon#supervisord: xcvrd   File "/usr/bin/xcvrd", line 683, in <module>
    Nov 18 00:57:55.488171 sonic INFO pmon#supervisord: xcvrd     sys.exit(main())
    Nov 18 00:57:55.488171 sonic INFO pmon#supervisord: xcvrd   File "/usr/bin/xcvrd", line 606, in main
    Nov 18 00:57:55.488188 sonic INFO pmon#supervisord: xcvrd     rc = post_port_sfp_info_to_db(logical_port, int_tbl)
    Nov 18 00:57:55.488188 sonic INFO pmon#supervisord: xcvrd   File "/usr/bin/xcvrd", line 265, in post_port_sfp_info_to_db
    Nov 18 00:57:55.488206 sonic INFO pmon#supervisord: xcvrd     port_info_dict = platform_sfputil.get_transceiver_info_dict(physical_port)
    Nov 18 00:57:55.488206 sonic INFO pmon#supervisord: xcvrd   File "/usr/local/lib/python2.7/dist-packages/sonic_platform_base/sonic_sfp/sfputilbase.py", line 726, in get_transceiver_info_dict
    Nov 18 00:57:55.501232 sonic INFO pmon#supervisord: xcvrd     sfp_vendor_pn_raw = self._read_eeprom_specific_bytes(sysfsfile_eeprom, (offset + OSFP_VENDOR_PN_OFFSET), XCVR_VENDOR_PN_WIDTH)
    Nov 18 00:57:55.501232 sonic INFO pmon#supervisord: xcvrd   File "/usr/local/lib/python2.7/dist-packages/sonic_platform_base/sonic_sfp/sfputilbase.py", line 314, in _read_eeprom_specific_bytes
    Nov 18 00:57:55.501390 sonic INFO pmon#supervisord: xcvrd     print("Error: reading sysfs file %s" % sysfs_sfp_i2c_client_eeprom_path)
    Nov 18 00:57:55.501390 sonic INFO pmon#supervisord: xcvrd NameError: global name 'sysfs_sfp_i2c_client_eeprom_path' is not defined
    
    Signed-off-by: Dante Su <dante.su@broadcom.com>

--- sonic_platform_base/sonic_sfp/sfputilbase.py
+++ sonic_platform_base/sonic_sfp/sfputilbase.py
@@ -311,7 +311,7 @@ class SfpUtilBase(object):
             sysfsfile_eeprom.seek(offset)
             raw = sysfsfile_eeprom.read(num_bytes)
         except IOError:
-            print("Error: reading sysfs file %s" % sysfs_sfp_i2c_client_eeprom_path)
+            print("Error: reading sysfs file %s" % sysfsfile_eeprom.name)
             return None
 
         try:
